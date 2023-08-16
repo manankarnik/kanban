6471e34e97a731a47194c8af0654e15acb69d314
Manan Karnik <manankarnik03@gmail.com>
2023-08-16 16:25:28 +0530

    refactor: move search bar to seperate component

---
+++ app/_components/SearchBar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import useInput from "../_hooks/useInput";
+import { FaSearch } from "react-icons/fa";
+
+export default function SearchBar() {
+  const [search, setSearch] = useInput("");
+  return (
+    <div className="flex gap-2 items-center w-full rounded-md border-0 p-2 text-black dark:text-white bg-gray-200 dark:bg-gray-600 ring-1 ring-inset ring-gray-300 dark:ring-gray-800 leading-6 focus-within:ring-2 focus-within:ring-teal-700 dark:focus-within:ring-teal-600">
+      <FaSearch className="text-gray-400" />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search"
+        value={search}
+        onChange={setSearch}
+        className="bg-transparent placeholder:text-gray-400 outline-0 font-normal"
+      />
+    </div>
+  );
+}
