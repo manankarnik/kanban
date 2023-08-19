1353503afafcbb4a03ecbe9add0c09aaf6a34cd2
Manan Karnik <manankarnik03@gmail.com>
2023-08-20 00:47:15 +0530

    feat: make cards draggable across columns

--- app/_components/KanbanContainer.tsx
+++ app/_components/KanbanContainer.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 import { FaPlus } from "react-icons/fa";
 import KanbanCard from "./KanbanCard";
 
-function KanbanContainer({ title }: { title: String }) {
-  const [cards, setCards] = useState([
-    { id: "1", text: "Card 1" },
-    { id: "2", text: "Card 2" },
-    { id: "3", text: "Card 3" },
-  ]);
+function KanbanContainer({
+  title,
+  elements,
+}: {
+  title: String;
+  elements: Array<{ id: string; text: string }>;
+}) {
   return (
     <div className="p-4 w-full rounded-lg bg-gray-200 dark:bg-gray-700">
       <div className="mb-2 flex justify-between items-center text-end font-semibold">
@@ -17,39 +18,27 @@ function KanbanContainer({ title }: { title: String }) {
           <FaPlus />
         </button>
       </div>
-      <DragDropContext
-        onDragEnd={(result) => {
-          if (!result.destination) {
-            return;
-          }
-          const newCards = [...cards];
-          const [removed] = newCards.splice(result.source.index, 1);
-          newCards.splice(result.destination.index, 0, removed);
-          setCards(newCards);
-        }}
-      >
-        <Droppable droppableId="droppable">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {cards.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided) => (
-                    <div
-                      className="mb-4"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <KanbanCard text={item.text} />
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <Droppable droppableId={`${title}`}>
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {elements.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <div
+                    className="mb-4"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <KanbanCard text={item.text} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
       <button className="font-bold text-xl w-full flex px-4 py-2 gap-2 rounded-md justify-center items-center whitespace-nowrap text-white bg-teal-700 hover:bg-teal-600">
         <FaPlus className="inline w-4 h-4" /> Add task
       </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,76 @@
 "use client";
-
+import { useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import KanbanContainer from "./_components/KanbanContainer";
 
 function Page() {
-  const columns = ["Todo", "In Progress", "Review", "Testing", "Done"];
+  const [columns, setColumns] = useState([
+    {
+      title: "Todo",
+      elements: [
+        { id: "1", text: "Todo Card 1" },
+        { id: "2", text: "Todo Card 2" },
+        { id: "3", text: "Todo Card 3" },
+      ],
+    },
+    {
+      title: "In Progress",
+      elements: [
+        { id: "4", text: "In Progres Card 1" },
+        { id: "5", text: "In Progres Card 2" },
+        { id: "6", text: "In Progres Card 3" },
+      ],
+    },
+    {
+      title: "Review",
+      elements: [
+        { id: "7", text: "Review Card 1" },
+        { id: "8", text: "Review Card 2" },
+        { id: "9", text: "Review Card 3" },
+      ],
+    },
+    {
+      title: "Testing",
+      elements: [
+        { id: "10", text: "Testing Card 1" },
+        { id: "11", text: "Testing Card 2" },
+        { id: "12", text: "Testing Card 3" },
+      ],
+    },
+    {
+      title: "Done",
+      elements: [
+        { id: "13", text: "Done Card 1" },
+        { id: "14", text: "Done Card 2" },
+        { id: "15", text: "Done Card 3" },
+      ],
+    },
+  ]);
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+    const newColumns = [...columns];
+    const src = newColumns.filter(
+      (element) => element.title == result.source.droppableId,
+    )[0];
+    const removed = src.elements.splice(result.source.index, 1)[0];
+    newColumns[newColumns.indexOf(src)] = src;
+    const dest = newColumns.filter(
+      (element) => element.title == result.destination!.droppableId,
+    )[0];
+    dest.elements.splice(result.destination!.index, 0, removed);
+    newColumns[newColumns.indexOf(dest)].elements = dest.elements;
+    setColumns(newColumns);
+  };
   return (
     <main className="flex py-4 gap-4">
-      {columns.map((title) => (
-        <KanbanContainer key={title} title={title} />
-      ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        {columns.map((column) => (
+          <KanbanContainer key={column.title} {...column} />
+        ))}
+      </DragDropContext>
     </main>
   );
 }
