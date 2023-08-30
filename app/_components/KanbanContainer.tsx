562c1650dd9db3860990a05a69136bc49eade072
Manan Karnik <manankarnik03@gmail.com>
2023-08-30 12:40:57 +0530

    feat: add tasks

--- app/_components/KanbanContainer.tsx
+++ app/_components/KanbanContainer.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Draggable, DraggableProvided, Droppable } from "react-beautiful-dnd";
 import { FaPlus, FaArrowsAlt } from "react-icons/fa";
 import KanbanCard, { Card } from "./KanbanCard";
+import PopupCard from "./PopupCard";
+import useInput from "../_hooks/useInput";
+import { ContainersContext } from "./Home";
+import { v4 as uuidv4 } from "uuid";
 
 export type Container = {
   id: string;
@@ -19,6 +23,21 @@ function KanbanContainer({
   draggableProps,
   dragHandleProps,
 }: KanbanContainerProps) {
+  const [showPopup, setShowPopup] = useState(false);
+  const [taskName, onInputTaskName, setTaskName] = useInput("");
+  const { containers, setContainers } = useContext(ContainersContext);
+  const addItem = () => {
+    const newContainers = [...containers];
+    newContainers
+      .find((container) => container.id == id)!
+      .elements.push({ id: uuidv4(), text: taskName });
+    setContainers(newContainers);
+    closePopup();
+  };
+  const closePopup = () => {
+    setTaskName("");
+    setShowPopup(false);
+  };
   return (
     <div
       ref={innerRef}
@@ -56,10 +75,31 @@ function KanbanContainer({
           </div>
         )}
       </Droppable>
-      <button className="font-bold text-xl w-full flex px-4 py-2 gap-2 rounded-md justify-center items-center whitespace-nowrap text-white bg-teal-700 hover:bg-teal-600">
+      <button
+        className="font-bold text-xl w-full flex px-4 py-2 gap-2 rounded-md justify-center items-center whitespace-nowrap text-white bg-teal-700 hover:bg-teal-600"
+        onClick={() => setShowPopup(true)}
+      >
         <FaPlus className="inline w-4 h-4" />
         <span>Add task</span>
       </button>
+      {showPopup ? (
+        <PopupCard title="Add Task" closePopup={closePopup} addItem={addItem}>
+          <div className="my-4 flex gap-4 items-center">
+            <label className="text-lg" htmlFor="taskName">
+              Task Name
+            </label>
+            <input
+              className="rounded-md border-0 p-2 text-black dark:text-white bg-gray-200 dark:bg-gray-600 ring-1 ring-inset ring-gray-300 dark:ring-gray-800 leading-6 focus-within:ring-2 focus-within:ring-teal-700 dark:focus-within:ring-teal-600 outline-0"
+              type="text"
+              name="taskName"
+              id="taskName"
+              autoFocus={true}
+              value={taskName}
+              onInput={onInputTaskName}
+            />
+          </div>
+        </PopupCard>
+      ) : null}
     </div>
   );
 }
